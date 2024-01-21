aacfcda8ed4c8a693a5cefffc7bbc045d6c739ea
redius01 <uju1346@gmail.com>
2024-01-21 16:37:11 +0900

    MOMENT Clone Coding

--- js/greetings.js
+++ js/greetings.js
@@ -27,40 +27,45 @@ function onLoginSubmit(event) {
         alert('입력 가능한 글자수는 최대 8자입니다. \n입력 내용을 수정해주세요.');
         loginInput.value = ""; // 입력 필드를 초기화
         loginInput.focus(); // 입력 필드에 포커스를 다시 맞춤
-        return;
+    } else {
+        localStorage.setItem(USERNAME_KEY, username); // 로컬 스토리지에 사용자 이름 저장
+        paintGreetings(username); // 인삿말 표시
     }
-    localStorage.setItem(USERNAME_KEY, username); // 로컬 스토리지에 사용자 이름 저장
-    paintGreetings(username); // 인삿말 표시
 }
+
 
 // 인삿말 표시 함수
 function paintGreetings(username) {
-    before.forEach(element => {
-        element.classList.add(HIDDEN_CLASSNAME);
-        greeting.classList.remove(INLINE_BLOCK);
-        goalList.classList.remove(FLEX__CLASSNAME);
-    });
     greeting.innerText = `Hello, ${username}.`; // 인삿말 설정
-    after.forEach(element => {
-        // after 클래스를 가진 모든 요소에서 HIDDEN_CLASSNAME 클래스를 제거
-        element.classList.remove(HIDDEN_CLASSNAME);
-    });
+    updateVisibility(before, true); // 로그인 전 요소들을 숨김
+    updateVisibility(after, false); // 로그인 후 요소들을 표시
     greeting.classList.add(INLINE_BLOCK);
     goalList.classList.add(FLEX__CLASSNAME);
-    quote.classList.add(FLEX__CLASSNAME);// quote에 플렉스 적용
+    quote.classList.add(FLEX__CLASSNAME);
 }
+
+// 요소 목록의 가시성을 갱신하는 함수
+function updateVisibility(elements, hidden) {
+    elements.forEach(element => {
+        element.classList[hidden ? 'add' : 'remove'](HIDDEN_CLASSNAME);
+    });
+
+    quote.classList[hidden ? 'remove' : 'add'](FLEX__CLASSNAME);
+}
+
 
 // 저장된 사용자 이름 확인
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
     // 사용자 이름이 저장되지 않았으면 로그인 폼 표시
-    before.forEach(element => {
-        element.classList.remove(HIDDEN_CLASSNAME);
-    });
+    updateVisibility(before, false);
+    quote.classList.remove(FLEX__CLASSNAME)
     loginForm.addEventListener("submit", onLoginSubmit);
 
 } else {
     // 저장된 사용자 이름이 있으면 인삿말 표시
     paintGreetings(savedUsername);
+
 }
+
