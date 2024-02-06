b864848a5c8be6435f29dbd202b039a4b340ca21
redius01 <uju1346@gmail.com>
2024-02-06 20:08:05 +0900

    MOMENT Clone Coding

--- js/background.js
+++ js/background.js
@@ -32,25 +32,29 @@ const mImages = [
 const mobileImg = document.querySelector("#mobileImage")
 const bgImages = document.querySelector("#backgroundImage")
 
+let WideScreen = window.innerWidth > 767;
 
 function updateBackgroundImage() {
-
     // 화면의 너비에 따라 적절한 배경 이미지 선택 및 표시
-    if (window.innerWidth <= 767) {
+    if (WideScreen && window.innerWidth <= 767) {
         // 모바일 화면
         const mRandomImg = mImages[Math.floor(Math.random() * mImages.length)];
         mobileImg.src = `img/${mRandomImg}`;
         bgImages.style.display = "none";
+        mobileImg.style.display = "block";
+        WideScreen = false;
     } else {
         // 데스크톱 화면
         const randomImages = images[Math.floor(Math.random() * images.length)];
         bgImages.src = `img/${randomImages}`;
         mobileImg.style.display = "none";
+        bgImages.style.display = "block";
+        WideScreen = true;
     }
 }
 
 // 페이지 로드 시점에 이미지 업데이트
 updateBackgroundImage();
 
-// 브라우저 창 크기 변경 시점에 이미지 업데이트
+// 창 크기 변경 시점에 이미지 업데이트
 window.addEventListener('resize', updateBackgroundImage);
